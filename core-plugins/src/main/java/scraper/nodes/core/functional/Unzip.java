ca9c519cfba1992146f066a152e609d2c003cac6
Albert Schimpf <git@schimpf.link>
2021-03-18 15:33:41 +0100

    Zip unzip properly implemented

--- core-plugins/src/main/java/scraper/nodes/core/functional/Unzip.java
+++ core-plugins/src/main/java/scraper/nodes/core/functional/Unzip.java
@@ -9,30 +9,34 @@ import scraper.api.node.type.FunctionalNode;
 import scraper.api.template.L;
 import scraper.api.template.T;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 /**
- * Unzips
+ * Unzips a list of pairs
  */
 @NodePlugin("0.0.1")
-public class UnzipSingle<A,B> implements FunctionalNode {
+public class Unzip<A,B> implements FunctionalNode {
 
     /** zipped list */
     @FlowKey(mandatory = true)
-    private final T<ZipList<A,B>.Pair> pair = new T<>(){};
+    private final T<List<Zip<A,B>.Pair>> pair = new T<>(){};
 
     /** unzip list 1 */
     @FlowKey(mandatory = true)
-    private final L<A> l1 = new L<>(){};
+    private final L<List<A>> l1 = new L<>(){};
 
     /** unzip list 2 */
     @FlowKey(mandatory = true)
-    private final L<B> l2 = new L<>(){};
+    private final L<List<B>> l2 = new L<>(){};
 
 
     @Override
     public void modify(@NotNull FunctionalNodeContainer n, @NotNull final FlowMap o) {
-        ZipList<A,B>.Pair zipped = o.eval(this.pair);
-
-        o.output(this.l1, zipped.fst);
-        o.output(this.l2, zipped.snd);
+        List<Zip<A,B>.Pair> unzipped = o.eval(this.pair);
+        List<A> first = unzipped.stream().map(z -> z.fst).collect(Collectors.toList());
+        List<B> second = unzipped.stream().map(z -> z.snd).collect(Collectors.toList());
+        o.output(this.l1, first);
+        o.output(this.l2, second);
     }
 }
